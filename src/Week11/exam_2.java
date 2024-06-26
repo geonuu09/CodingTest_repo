b71f1608440fbadf7343fc9313fe42c755b2c606
LeeGeonu <rjsdndl909@gmail.com>
2024-06-26 10:59:57 +0900

    second commit2

--- src/Week11/exam_2.java
+++ src/Week11/exam_2.java
@@ -14,6 +14,10 @@ import java.util.List;
  - 숫자의 길이가 다르고, 앞자리가 같을 경우 더 짧은 숫자가 앞에 온다. (10 이 104 보다 앞에 온다)
  */
 
+
+// 숫자 -> 문자열 -> 정렬 -> 숫자
+    // 숫자 O(n), 문자열 0(n), 정렬 O(nlogN)
+    // 따라서 시간 복잡도 : O(nlogN)
 class Solution1 {
     public int[] solution(int n) {
         List<String> numbers = new ArrayList<>();
@@ -30,8 +34,42 @@ class Solution1 {
         return numbers.stream().mapToInt(s -> Integer.parseInt(s)).toArray();
     }
 }
+
+    // dfs로 구현
+    // 시간 복잡도 : O(n)
+class Solution2 {
+    List<Integer> res;
+    public int[] solution(int n) {
+        res = new ArrayList<>();
+        dfs(0,n);
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    void dfs(int num, int n) {
+        // 탈출 조건
+        if(num > n) {
+            return;
+        }
+        if(num > 0) {
+            res.add(num);
+        }
+
+        for(int i = 0; i < 10; i++) {
+            int newNum = num * 10 + i; // 자리올림
+            if(newNum == 0) { // 단, (0* 10+ 0 = 0) 탐색에서 제외
+                continue;
+            }
+            dfs(newNum, n);
+        }
+        /** 1 : 0 ~ 9
+         *  2 : 0 ~ 9
+         *   ...
+         */
+    }
+}
 public class exam_2 {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new Solution1().solution(15)));
+        System.out.println(Arrays.toString(new Solution2().solution(15)));
     }
 }
